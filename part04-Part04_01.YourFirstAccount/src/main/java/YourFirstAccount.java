f1f588880e9d8722839b538463a21238a219a8c5
dauphongxd <121737251+dauphongxd@users.noreply.github.com>
2024-01-05 21:54:12 -0600

    Add files via upload

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account phongsAccount = new Account("Phong's Account", 100.0);
+        phongsAccount.deposit(20.0);
+        System.out.println(phongsAccount);
+    }
+}
